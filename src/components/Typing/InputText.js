fb779d95b3fa14351598267c5b4e96b15e4277a6
jmossesgeld <jmossesgeld@gmail.com>
2021-11-05 00:11:32 +0800

    change height to be relative to viewport

--- src/components/Typing/InputText.js
+++ src/components/Typing/InputText.js
@@ -1,14 +1,18 @@
 import classes from "./InputText.module.css";
+import React, { useRef } from "react";
+
 import { useSelector, useDispatch } from "react-redux";
 
 const InputText = () => {
   const inputText = useSelector((state) => state.input);
   const dispatch = useDispatch();
+  const textArea = useRef();
 
   const inputChangeHandler = (e) => {
     dispatch({ type: "startTimer" });
     dispatch({ type: "setInput", value: e.target.value });
     if (e.target.value.slice(-1) === " ") {
+      textArea.current.scrollTop = textArea.current.scrollHeight;
       dispatch({ type: "validate" });
     }
   };
@@ -21,8 +25,9 @@ const InputText = () => {
         className={classes.style}
         value={inputText}
         onChange={inputChangeHandler}
+        ref={textArea}
       ></textarea>
-      <em style={{ marginBottom:"2rem", display: "block", fontSize: "0.7rem" }}>
+      <em style={{ marginBottom: "2rem", display: "block", fontSize: "0.7rem" }}>
         Press Up and Down to cycle through the words entered
       </em>
     </div>
--- src/components/Typing/Words.js
+++ src/components/Typing/Words.js
@@ -9,12 +9,17 @@ const Words = () => {
   const validations = useSelector((state) => state.validations);
 
   useEffect(() => {
-    const wordsApi = "https://random-word-api.herokuapp.com/word?number=20";
-    fetch(wordsApi)
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: "setWords", value: data });
-      });
+    // const wordsApi = "https://random-word-api.herokuapp.com/word?number=20";
+    // fetch(wordsApi)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     dispatch({ type: "setWords", value: data });
+    //   });
+    dispatch({
+      type: "setWords",
+      value:
+        "Because of the relation between the manifold and natural causes, let us suppose that the architectonic of human reason constitutes the whole content for, in other words, our concepts; however, philosophy (and there can be no doubt that this is true) is just as necessary as the things in themselves. The noumena prove the validity of, as I have elsewhere shown, our understanding, and our concepts, in the study of pure reason, can never, as a whole, furnish a true and demonstrated science, because, like the thing in itself, they stand in need to speculative principles. In the case of the transcendental unity of apperception, the reader should be careful to observe that pure logic, in respect of the intelligible character, excludes the possibility of our ideas, as is shown in the writings of Galileo.".split(" "),
+    });
   }, [dispatch]);
 
   return (
