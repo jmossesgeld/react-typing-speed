d3de027e4ccaadd2bc7b1b3d6cb50cabcfb59b67
jmossesgeld <jmossesgeld@gmail.com>
2021-11-13 16:08:39 +0800

    add standalone option to react-typing-speed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import store from "./store/store";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// ---UNCOMMENT THE CODE BELOW TO USE THIS AS A STAND-ALONE REACT APP---
+// import ReactDOM from "react-dom";
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const ReactTypingSpeed = () => {
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+};
+
+export default ReactTypingSpeed
