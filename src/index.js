1de21916721e21f29d87740876d003896de7dbd1
jmossesgeld <jmossesgeld@gmail.com>
2021-11-13 16:39:10 +0800

    change render method

--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import "./index.css";
 import App from "./App";
 import store from "./store/store";
 import { Provider } from "react-redux";
-
-//---UNCOMMENT THE CODE BELOW TO USE THIS AS A STAND-ALONE REACT APP---
 import ReactDOM from "react-dom";
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
 
 const ReactTypingSpeed = () => {
   return (
@@ -25,4 +15,9 @@ const ReactTypingSpeed = () => {
   );
 };
 
-export default ReactTypingSpeed
+ReactDOM.render(
+  <ReactTypingSpeed/>,
+  document.getElementById("root")
+);
+
+export default ReactTypingSpeed;
