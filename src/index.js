27da7801a19f3c7d16f30d3e3d7bf3de3d802dcd
jmossesgeld <jmossesgeld@gmail.com>
2021-11-13 16:21:10 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import App from "./App";
 import store from "./store/store";
 import { Provider } from "react-redux";
 
-// ---UNCOMMENT THE CODE BELOW TO USE THIS AS A STAND-ALONE REACT APP---
-// import ReactDOM from "react-dom";
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+//---UNCOMMENT THE CODE BELOW TO USE THIS AS A STAND-ALONE REACT APP---
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 const ReactTypingSpeed = () => {
   return (
