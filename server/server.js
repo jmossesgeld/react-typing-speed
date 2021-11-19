247542ee8c2e3395ee511d9d38880796ffbe2d5e
jmossesgeld <jmossesgeld@gmail.com>
2021-11-19 11:49:15 +0800

    create env file

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
